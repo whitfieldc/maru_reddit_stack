34f3b63252d82214da35cc139d76181d43bde85c
Charles <whitfield.cw@gmail.com>
2015-09-24 23:12:07 -0700

    add working 'hello world' json

--- lib/maru_reddit_stack.ex
+++ lib/maru_reddit_stack.ex
@@ -1,2 +1,18 @@
-defmodule MaruRedditStack do
+defmodule MaruRedditStack.Router.Homepage do
+  use Maru.Router
+
+  get do
+    %{ hello: :world }
+  end
+end
+
+defmodule MaruRedditStack.API do
+  use Maru.Router
+
+  mount MaruRedditStack.Router.Homepage
+
+  rescue_from :all do
+    status 500
+    "Server Error"
+  end
 end
