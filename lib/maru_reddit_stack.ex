12245086cf91cbdd71b935dc6799b72ce57850b1
Charles <whitfield.cw@gmail.com>
2015-09-27 22:53:53 -0700

    add broken HTTPoison get

--- lib/maru_reddit_stack.ex
+++ lib/maru_reddit_stack.ex
@@ -4,6 +4,19 @@ defmodule MaruRedditStack.Router.Homepage do
   get do
     %{ hello: :world }
   end
+
+  get "/:route" do
+    # Plug.Conn.send_resp(conn, 200, params[:route] )
+    # HTTPoison.start
+    # resp = HTTPoison.get!("https://reddit.com/r/" <> params[:route] <> ".json", ["User-Agent": "app:com.marureddit.stack (by /u/retiredhipster)"], [follow_redirect: true])
+    # b = Poison.decode! resp.body
+    # Plug.Conn.send_resp(conn, 200, resp )
+    resp = HTTPoison.get("http://httpbin.org/get", ["User-Agent": "app:com.marureddit.stack (by /u/retiredhipster)"], [follow_redirect: true])
+    # IO.puts "hi"
+    IO.inspect resp
+    # Plug.Conn.send_resp(conn, 200, resp.body )
+    # %{ hello: resp }
+  end
 end
 
 defmodule MaruRedditStack.API do
@@ -17,4 +30,4 @@ defmodule MaruRedditStack.API do
   end
 end
 
-"http://reddit.com/r/" <> subreddit <> ".json"
+# "http://reddit.com/r/" <> subreddit <> ".json"
